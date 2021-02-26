91827eae0fff228606b71d20a523c9ade8602c0a
david12997 <david12997@outlook.com>
2021-02-25 19:14:43 -0500

    arreglando menu

--- js/menuuser.js
+++ js/menuuser.js
@@ -101,7 +101,17 @@ menu.addEventListener('click',()=>{
     
     `);
 
+    $('.contenedor-cate').empty();
+    $('.contenedor-cate').append(`
+    <div class="d-flex justify-content-center">
+    <div style="width: 5rem; height: 5rem;" class="spinner-border text-success" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+  </div>
     
+    
+    `);
+
     fetch('api/interfaces/categorias.php')
     .then(response=>response.json())
     .then(response=>{
